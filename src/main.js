3ffff1de678dbbf27e97a8efc5892d9e88de06a7
Jiameng <55512849+jiamengb@users.noreply.github.com>
2020-03-09 19:39:22 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import fusion_charts from './plugins/fusionCharts';
+import 'echarts/lib/chart/map'
+import 'echarts/lib/component/visualMap'
+import 'echarts/lib/component/title'
 
 Vue.config.productionTip = false;
 
