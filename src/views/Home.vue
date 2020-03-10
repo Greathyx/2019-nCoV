72d98734b886457f793ff5adee3ab4e990aaae6e
Greathyx <hyx_GreatExpectations@yahoo.com>
2020-03-10 07:11:05 +0100

    attempt commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,5 @@
 <template>
   <v-app id="inspire">
-<!--    <v-navigation-drawer-->
-<!--            v-model="drawer"-->
-<!--            app-->
-<!--            clipped-->
-<!--    >-->
-<!--      <v-list dense>-->
-<!--        <v-list-item link>-->
-<!--          <v-list-item-action>-->
-<!--            <v-icon>mdi-map-search</v-icon>-->
-<!--          </v-list-item-action>-->
-<!--          <v-list-item-content>-->
-<!--            <v-list-item-title>Current Situation</v-list-item-title>-->
-<!--          </v-list-item-content>-->
-<!--        </v-list-item>-->
-
-<!--        <v-list-item link>-->
-<!--          <v-list-item-action>-->
-<!--            <v-icon>mdi-chart-line</v-icon>-->
-<!--          </v-list-item-action>-->
-<!--          <v-list-item-content>-->
-<!--            <v-list-item-title>Future Prediction</v-list-item-title>-->
-<!--          </v-list-item-content>-->
-<!--        </v-list-item>-->
-<!--      </v-list>-->
-<!--    </v-navigation-drawer>-->
-
     <v-content>
       <v-container
               fluid
@@ -58,7 +32,6 @@
         },
 
         data: () => ({
-            drawer: true,
         }),
     };
 </script>
