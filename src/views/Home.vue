a9df61e27f3c61ab2646e4d3555b92958627dc9d
Jiameng <55512849+jiamengb@users.noreply.github.com>
2020-03-10 03:56:13 +0100

    Add files via upload
    醒了再改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,106 +1,64 @@
 <template>
   <v-app id="inspire">
-    <v-content>
-      <div class="box">
-        <div class="fill-height sidebar">
-          <div class="total" style="background-color: #FFCDD2">
-            <h1>111,362</h1>
-            <h4>Total Confirmed</h4>
-          </div>
-          <div class="total" style="background-color: #E0E0E0">
-            <h1>3,892</h1>
-            <h4>Total Deaths</h4>
-          </div>
-          <div class="total" style="background-color: #C8E6C9">
-            <h1>62,392</h1>
-            <h4>Total Recovered</h4>
-          </div>
-          <div class="countries">
-            <h5 style="margin: 10px auto">Confirmed Cases by Country</h5>
-            <div style="display: flex; flex-direction: column; align-items: flex-start; padding: 0 25px">
-              <p v-for="item in confirmed_data" v-bind:key="item.name">
-                <span style="color: #F44336; font-weight: bold">{{item.value}}</span> {{item.name}}
-              </p>
-            </div>
-          </div>
-        </div>
+<!--    <v-navigation-drawer-->
+<!--            v-model="drawer"-->
+<!--            app-->
+<!--            clipped-->
+<!--    >-->
+<!--      <v-list dense>-->
+<!--        <v-list-item link>-->
+<!--          <v-list-item-action>-->
+<!--            <v-icon>mdi-map-search</v-icon>-->
+<!--          </v-list-item-action>-->
+<!--          <v-list-item-content>-->
+<!--            <v-list-item-title>Current Situation</v-list-item-title>-->
+<!--          </v-list-item-content>-->
+<!--        </v-list-item>-->
 
-        <div class="main">
-          <WorldMap/>
-        </div>
-      </div>
+<!--        <v-list-item link>-->
+<!--          <v-list-item-action>-->
+<!--            <v-icon>mdi-chart-line</v-icon>-->
+<!--          </v-list-item-action>-->
+<!--          <v-list-item-content>-->
+<!--            <v-list-item-title>Future Prediction</v-list-item-title>-->
+<!--          </v-list-item-content>-->
+<!--        </v-list-item>-->
+<!--      </v-list>-->
+<!--    </v-navigation-drawer>-->
+
+    <v-content>
+      <v-container
+              fluid
+              fill-height
+      >
+        <v-layout
+                align-center
+                justify-center
+        >
+          <v-flex shrink>
+            <Map/>
+<!--            <Chart/>-->
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
+
   </v-app>
 </template>
 
 <script>
-    // import Map from "../components/Map";
+    import Map from "../components/Map";
     // import Chart from "../components/Chart";
-    import WorldMap from "../components/WorldMap";
-    import data from '../data/data.json'
-    import {getName} from '../data/name'
-
-    const countries = data.areaTree;
-    let confirmed_data = [];
-
-    countries.forEach(p => {
-        if (getName(p.name)) {
-            confirmed_data.push({
-                name: getName(p.name),
-                value: p.total.confirm
-            })
-        }
-    });
 
     export default {
         name: 'Home',
         components: {
-            WorldMap,
-            // Map
+            Map
             // Chart
         },
 
         data: () => ({
-            confirmed_data: confirmed_data
+            drawer: true,
         }),
     };
 </script>
-
-<style>
-  .box {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    padding: 10px;
-  }
-
-  .sidebar {
-    display: flex;
-    flex-direction: column;
-    flex-basis: 250px;
-    min-width: 250px; /*确保当页面很小的时候，左侧变小*/
-    flex-grow: 0;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .main {
-    flex-grow: 1;
-  }
-
-  .total {
-    height: 80px;
-    width: 100%;
-    flex-grow: 0;
-    margin-bottom: 6px;
-  }
-
-  .countries {
-    width: 100%;
-    background-color: bisque;
-    flex-grow: 1;
-    overflow: scroll;
-    max-height: calc(70vh);
-  }
-</style>
