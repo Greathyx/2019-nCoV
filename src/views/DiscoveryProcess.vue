e0de843fc76d0b058c484eb4ecc8371efc8939aa
Greathyx <hyx_GreatExpectations@yahoo.com>
2020-03-22 19:53:29 +0100

    improve layout 3

--- src/views/DiscoveryProcess.vue
+++ src/views/DiscoveryProcess.vue
@@ -5,8 +5,7 @@
             :options='options'
             :init-options='initOptions'
             autoresize
-            style="width: 90%; height: calc(95vh); margin: 0 auto;"
-            class="fill-height"
+            class="fill-height my_echart"
     />
   </v-content>
 </template>
@@ -37,5 +36,14 @@
 <style>
   .chart_container {
     background-color: #E0F2F1;
+  }
+
+  .my_echart {
+    width: 90%;
+    height: calc(85vh);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 </style>
