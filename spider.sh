af7540c38f2b08e4b4dcadbb28e706f96523bddf
Jiameng <55512849+jiamengb@users.noreply.github.com>
2020-03-24 00:15:58 +0100

    Delete spider.sh

--- spider.sh
+++
@@ -1,2 +0,0 @@
-curl "https://raw.githubusercontent.com/canghailan/Wuhan-2019-nCoV/master/Wuhan-2019-nCoV.json" >> "./src/data/data.json"
-curl "https://raw.githubusercontent.com/canghailan/Wuhan-2019-nCoV/master/Wuhan-2019-nCoV.json" >> "./data.json"
