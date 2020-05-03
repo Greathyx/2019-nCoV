02df78eb8fa7e741f3303a45a9c2d193956bf57b
Greathyx <hyx_GreatExpectations@yahoo.com>
2020-05-03 14:15:26 +0200

    change date

--- src/data/generateECharts.js
+++ src/data/generateECharts.js
@@ -12,11 +12,11 @@ import {getName, CODE, getCode} from './name'
 
 // generate date of today
 const today = new Date();
-// const dd = String(today.getDate()).padStart(2, '0');
-// const mm = String(today.getMonth() + 1).padStart(2, '0'); // Jan is 0
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); // Jan is 0
 const yyyy = today.getFullYear();
-// const today_date = yyyy + '-' + mm + '-' + dd;
-const today_date = '2020-04-01';
+const today_date = yyyy + '-' + mm + '-' + dd;
+// const today_date = '2020-04-01';
 
 // 在北京时间 12 点前用前一天的数据替代当天数据
 // 大概还有更好的写法吧比如比较 data.json 的 md5 ……
